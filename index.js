4fe51f2431b07aa059546feafce62e99931f80e0
abdullohasia <tolaganova624@gmail.com>
2023-10-20 20:04:56 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,66 @@
+
+
+ //Problem #180
+
+
+// function MinMax() {
+//   console.log(Math.max(1, 2, 10, 5, 6, 7));
+//   console.log(Math.min(1, 2, 10, 5, 6, 7));
+// }
+
+// MinMax();
+
+
+// Problem #155
+
+// let Numbers = [2, 6, 9, 7, 5];
+
+// console.log(Numbers.shift());
+// console.log(Numbers[1]);
+// console.log(Numbers.pop());
+
+
+// 157
+
+// function abdulloh (arr = [ 1, 2, 3, 4, 5, 6, 77, 88, 99, ]) {
+//     let juft = 0 ;
+//     for (let i = 0; i < arr.length; i++) {
+//         if( arr[i] % 2 === 0 ) {
+//             [arr[i], arr[juft] ] = [arr[juft], arr[i] ]
+//             juft++
+//         }
+//     }
+//     console.log(arr)
+// }
+// abdulloh()
+
+
+
+//167 
+
+// function storeAndSumEvenNumbers(array) {
+//     const evenNumbers = array.filter(number => number % 2 === 0);
+//     const sum = evenNumbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+//     return sum;
+//   }
+  
+//   const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//   const result = storeAndSumEvenNumbers(numbers);
+  
+//   console.log(result);
+
+// const numbers2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let sum = 0;
+
+// numbers2.forEach((element) => {
+//     if(element % 2 == 0){
+//         sum += element
+//     }else{
+//         console.error()
+//     }
+    
+// })
+// console.log(sum);
+
+
+  
